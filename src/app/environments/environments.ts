5b727085a7d55db0fe84f7b037b65907d1ca0d0e
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-07-28 09:42:27 -0300

    Criando Estrutura do projeto

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+    API_URL:"https://sdw-2023-prd.up.railway.app/users/1" /*tODA VEZ QUE EU QUISER CHAMAR MINHA api
+    eu não preciso mais ficar lembrando o link, basta eu chamar essa constante API_URL.*/ 
+}
