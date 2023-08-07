65faf13978640a0561e66d3772a3124fd6da3aa9
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-08-07 02:04:37 -0300

    Consumindo a API e Dados  dinâmicos no Card-box

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountDataModel } from 'src/app/model/accountDataModel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-box',
@@ -7,12 +8,17 @@ import { AccountDataModel } from 'src/app/model/accountDataModel';
   styleUrls: ['./card-box.component.css']
 })
 export class CardBoxComponent implements OnInit {
+
+  // fazendo a injeção de depêndencia
+  constructor(private service: CardsService){
+
+  }
   
 
   /* Aqui eu to falando que no meu componente eu tenho uma forma de entrada de dados que segue a mesma 
   forma dos meus objetos */
   accountData: AccountDataModel = {
-    name: "ISAEL PAI DE FAMILIA",
+    name: "",
     // objeto account
     account: {
     agency: "696969",
@@ -25,7 +31,22 @@ export class CardBoxComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    /*aqui estou chamando os dados do meu método*/
+    this.getAccountData()
+  }
+
+  // função que vai chamar o meu service.
+  getAccountData(){
+    // Para usar o observable é necessario se inscrever nele através do subscribe
+    this.service.getCard().subscribe(data =>{
+      this.accountData.name = data.name
+      /* Basicamente estou atribuindo o valor do resultado que esta trazendo da api do data 
+      pro meu objeto accountData */ 
+      this.accountData.account.agency = data.account.agency
+      this.accountData.account.number = data.account.number
+      this.accountData.card.limit = data.card.limit
+      this.accountData.card.number = data.card.number.split(" ")[3]
+    })
   }
 
 }
